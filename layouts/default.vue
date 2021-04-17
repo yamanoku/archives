4ca07a883eafa72a3c4bf61c00f00edc6552190a
yamanoku <0910yama@gmail.com>
2021-04-17 19:58:35 +0900

    settings

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,44 @@
 <template>
   <div>
-    <Nuxt />
+    <global-header />
+    <nuxt />
+    <global-footer />
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+import GlobalHeader from '@/components/global/Header.vue'
+import GlobalFooter from '@/components/global/Footer.vue'
+
+export default Vue.extend({
+  components: {
+    GlobalHeader,
+    GlobalFooter,
+  },
+})
+</script>
+
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+html[lang='ja'] {
+  font-kerning: none;
 }
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+html:not([lang='ja']) body {
+  hyphens: auto;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+html[lang='ja'] body {
+  line-break: loose;
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+html[lang='ja'] main,
+html[lang='ja'] header,
+html[lang='ja'] nav,
+html[lang='ja'] footer {
+  max-width: 45rem;
 }
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+code.hljs {
+  padding: calc(var(--rhythm) * 3);
 }
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+img {
+  height: auto;
 }
 </style>
