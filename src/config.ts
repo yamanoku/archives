82fe7f9f897cc57b9ceaa6aea2b1cde806f5a705
yamanoku <0910yama@gmail.com>
2022-11-20 22:20:10 +0900

    add: suffix yamanoku.net

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'アーカイブ';
+export const SITE_TITLE = 'アーカイブ | yamanoku.net';
 export const SITE_DESCRIPTION =
   'このページはyamanokuこと大山奥人が書いてきた過去の記事やログを収集したページです。';
