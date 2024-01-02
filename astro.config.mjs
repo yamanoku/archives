ab03bbaf18419c9c23f7d4d469e2bb0abea2afb0
yamanoku <0910yama@gmail.com>
2024-01-02 14:10:05 +0900

    add: tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,23 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import resolveLayoutShiftPlugin from 'rehype-plugin-auto-resolve-layout-shift'
+import resolveLayoutShiftPlugin from 'rehype-plugin-auto-resolve-layout-shift';
 import lazyloadPlugin from 'rehype-plugin-image-native-lazy-loading';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://archives.yamanoku.net',
-  integrations: [sitemap()],
+  integrations: [sitemap(), tailwind()],
   markdown: {
     syntaxHighlight: 'prism',
     gfm: true,
-    rehypePlugins: [
-      [resolveLayoutShiftPlugin, { type: 'maxWidth', maxWidth: 900 }],
-      lazyloadPlugin,
-    ],
+    rehypePlugins: [[resolveLayoutShiftPlugin, {
+      type: 'maxWidth',
+      maxWidth: 900
+    }], lazyloadPlugin],
     remarkRehype: {
       footnoteLabel: '脚注',
-      footnoteBackLabel: 'コンテンツに戻る',
-    },
-  },
+      footnoteBackLabel: 'コンテンツに戻る'
+    }
+  }
 });
