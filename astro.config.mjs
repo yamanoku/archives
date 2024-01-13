9b7e9faf2e9533a5cffaa456367acee7ea213f6c
yamanoku <0910yama@gmail.com>
2024-01-13 15:02:30 +0900

    use svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import sitemap from '@astrojs/sitemap';
 import resolveLayoutShiftPlugin from 'rehype-plugin-auto-resolve-layout-shift';
 import lazyloadPlugin from 'rehype-plugin-image-native-lazy-loading';
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://archives.yamanoku.net',
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), svelte()],
   markdown: {
     syntaxHighlight: 'prism',
     gfm: true,
