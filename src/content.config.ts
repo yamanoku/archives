806fa2a4860d5eed78fec59c39f12615e13eb51d
yamanoku <0910yama@gmail.com>
2024-12-04 20:18:34 +0900

    del

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,6 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const archives = defineCollection({
-  // For content layer you no longer define a `type`
   loader: glob({ pattern: '**/*.md', base: './src/archives' }),
   schema: z.object({
     date: z.date(),
