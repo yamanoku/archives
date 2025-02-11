07ce3fb18f3041536e0b726f6636ab0a6dab13d2
yamanoku <0910yama@gmail.com>
2025-02-12 08:57:40 +0900

    prettier忘れ

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import resolveLayoutShiftPlugin from 'rehype-plugin-auto-resolve-layout-shift';
 import lazyloadPlugin from 'rehype-plugin-image-native-lazy-loading';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,13 +14,19 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: 'prism',
     gfm: true,
-    rehypePlugins: [[resolveLayoutShiftPlugin, {
-      type: 'maxWidth',
-      maxWidth: 900
-    }], lazyloadPlugin],
+    rehypePlugins: [
+      [
+        resolveLayoutShiftPlugin,
+        {
+          type: 'maxWidth',
+          maxWidth: 900,
+        },
+      ],
+      lazyloadPlugin,
+    ],
     remarkRehype: {
       footnoteLabel: '脚注',
-      footnoteBackLabel: 'コンテンツに戻る'
-    }
-  }
+      footnoteBackLabel: 'コンテンツに戻る',
+    },
+  },
 });
